fc0afc81a69b699f4e38c008aaf98faa9b857987
Hakuran <WBhlietue@gmail.com>
2023-04-30 10:34:11 +0800

    build

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import "../css/Header.css";
 import { Link } from "react-router-dom";
 import { GetTestLogo } from "./GetTestLogo";
+import { useState } from "react";
 
 export function Header() {
+  const [menuHide, setMenuHide] = useState(true);
   return (
     <div className="headerMain">
       <div className="headerMainLogoParent">
@@ -11,23 +13,40 @@ export function Header() {
         </div>
         <div className="headerMainLogoText">Apollo</div>
       </div>
-      <ul className="headerMainNav">
-        <li>
-          <Link className="headerMainNavLink headerMainUnderLineAni" to={"/"}>
-            <div>Нүүр</div>
-          </Link>
-        </li>
-        <li>
-          <Link className="headerMainNavLink headerMainUnderLineAni" to={"/orderList"}>
-            <div>Бидний тухай</div>
-          </Link>
-        </li>
-        <li>
-          <Link className="headerMainNavLink headerMainUnderLineAni" to={"/Test"}>
-            <div>Зөвөлгөө</div>
-          </Link>
-        </li>
-      </ul>
+      <div className="headerMainNav">
+        <ul style={{display:menuHide? "none":"block"}}>
+          <li>
+            <Link className="headerMainNavLink headerMainUnderLineAni" to={"/"}>
+              <div>Нүүр</div>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="headerMainNavLink headerMainUnderLineAni"
+              to={"/orderList"}
+            >
+              <div>Бидний тухай</div>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="headerMainNavLink headerMainUnderLineAni"
+              to={"/Test"}
+            >
+              <div>Зөвөлгөө</div>
+            </Link>
+          </li>
+        </ul>
+        <div
+          className="headerMainNavBtn"
+          style={{transform:"rotateZ("+(menuHide? 90:-90)+"deg)"}}
+          onClick={() => {
+            setMenuHide((i) => !i);
+          }}
+        >
+          &#8250;
+        </div>
+      </div>
       <div className="headerMainRight">
         <div className="headerMainLogin headerMainUnderLineAni">Нэвтрэх</div>
         <div className="headerMainRegister">
