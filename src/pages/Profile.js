efccf2d4932e4fd873357520f69e16f642763844
Hakuran <WBhlietue@gmail.com>
2023-04-30 16:39:53 +0800

    adas

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -11,57 +11,57 @@ export function Profile() {
                     <img src={logo}></img>
                 </div>
                 <div className='LeftContainer'>
-                    <h2 className="headerrr">Ерөнхий мэдээлэл</h2>
+                    <h2 className="headerrr">Ерөнхий мэдээлэл&nbsp; &nbsp; &nbsp; </h2>
                     <ul>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Овог">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Нэр">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Утасны дугаар">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="И-Мэйл">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Яаралтай үед холбогдох">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Боловсрол">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Гэрийн хаяг">
                             </input>
                         </li>
                     </ul>
                     <button className="edithBtn">Засварлах</button>
                 </div>
                 <div className='RightContainer'>
-                    <h2 className="headerrr">Нэмэлт мэдээлэл</h2>
+                    <h2 className="headerrr">Нэмэлт мэдээлэл&nbsp; &nbsp; &nbsp; </h2>
                     <ul>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Дэлнэрэнгүй мэдээлэл">
                             </input>
                         </li>
                         <li>
                             <img src={check}></img>
-                            <input type="textbox">
+                            <input type="textbox" placeholder="Чадвар">
                             </input>
                         </li>
 
